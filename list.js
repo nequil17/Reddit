8ca1690bf6678f4d9828aa753af84c7e4b68853d
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-07-13 11:33:24 -0700

    list works/backgrounds added

--- list.js
+++ list.js
@@ -1,10 +1,32 @@
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+var httpRequest = new XMLHttpRequest();
+var api = "https://www.reddit.com/r/dogpictures.json";
+
+httpRequest.open("GET", api);
+httpRequest.send();
+httpRequest.addEventListener("readystatechange", processRequest, false);
+
+function processRequest(e) {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        var data = e.target.response;
+        var parsedData = JSON.parse(data);
+        var posts = parsedData.data.children;
+        console.log(posts)
+
+        for(var i = 0; i < posts.length; i++) {
+            var heading = document.createElement('h2');
+            heading.innerText = posts[i].data.title;
+            document.body.appendChild(heading);
+
+            var link = document.createElement('a');
+            link.setAttribute('href', 'single.html', + '?url=' + posts[i].data.url);
+            link.style.width = '200px';
+            document.body.appendChild(link);
+            link.appendChild(heading);
+
+            var postBody = document.createElement('img');
+            postBody.setAttribute('src', posts[i].data.thumbnail);
+            document.body.appendChild(postBody);
+        }
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
 }
+
